f585ac53f073ecb16ab5a226f0c03053474eeb77
zengzhihua24 <765795635@qq.com>
2019-09-08 02:21:46 +0800

    001_2

--- src/_001_TwoSum.java
+++ src/_001_TwoSum.java
@@ -1,15 +1,26 @@
+import java.util.HashMap;
+
 public class _001_TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        int res[] = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    res[0] = i;
-                    res[1] = j;
-                    return res;
+                    return new int[]{i, j};
                 }
             }
+        }
+        return null;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        // <value, index>
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
         }
         return null;
     }
