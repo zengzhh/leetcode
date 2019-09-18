a53cb003cabe4e4a27b7e0aa40cfafec3eeb34d9
zengzhihua24 <765795635@qq.com>
2019-09-19 00:09:27 +0800

    021_2

--- src/_021_MergeTwoSortedLists.java
+++ src/_021_MergeTwoSortedLists.java
@@ -12,12 +12,33 @@ public class _021_MergeTwoSortedLists {
         }
     }
 
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode n0 = new ListNode(0);
+        ListNode p = n0;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) {
+            p.next = l1;
+        } else {
+            p.next = l2;
+        }
+        return n0.next;
+    }
+
     public static void main(String[] args) {
         int[] nodes1 = new int[]{2, 5, 6};
         int[] nodes2 = new int[]{3, 4};
         ListNode l1 = Util.createListNode(nodes1);
         ListNode l2 = Util.createListNode(nodes2);
-        ListNode l3 = new _021_MergeTwoSortedLists().mergeTwoLists(l1, l2);
+        ListNode l3 = new _021_MergeTwoSortedLists().mergeTwoLists2(l1, l2);
         Util.printNode(l3);
     }
 }
