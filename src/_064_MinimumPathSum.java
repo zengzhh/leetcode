df1a3f8b3248f54939b770554eb9afd0bfa403e1
zengzhihua24 <765795635@qq.com>
2019-10-18 02:49:49 +0800

    064

---
+++ src/_064_MinimumPathSum.java
@@ -0,0 +1,28 @@
+public class _064_MinimumPathSum {
+
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] = dp[j] + grid[i][j];
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] + grid[i][j];
+                } else {
+                    dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        Util.printMatrix(grid);
+        int min = new _064_MinimumPathSum().minPathSum(grid);
+        System.out.println(min);
+    }
+}
