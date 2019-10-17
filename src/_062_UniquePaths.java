aa3c44d35f16bac768ac5b2143d82609e6e12adf
zengzhihua24 <765795635@qq.com>
2019-10-18 00:55:49 +0800

    062

---
+++ src/_062_UniquePaths.java
@@ -0,0 +1,25 @@
+public class _062_UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) return 0;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int res = new _062_UniquePaths().uniquePaths(7, 3);
+        System.out.println(res);
+    }
+}
