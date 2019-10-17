0d6fd8394c5d0081e55bfbb16de127b1b8ffad53
zengzhihua24 <765795635@qq.com>
2019-10-18 01:27:32 +0800

    062_2

--- src/_062_UniquePaths.java
+++ src/_062_UniquePaths.java
@@ -3,19 +3,30 @@ public class _062_UniquePaths {
     public int uniquePaths(int m, int n) {
         if (m == 0 || n == 0) return 0;
         int[][] dp = new int[m][n];
-        dp[0][0] = 1;
         for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
             }
         }
         return dp[m - 1][n - 1];
+    }
+
+    public int uniquePaths2(int m, int n) {
+        if (m == 0 || n == 0) return 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
     }
 
     public static void main(String[] args) {
