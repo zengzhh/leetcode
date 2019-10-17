17d41b217e6ed2d5c80abb667303287feec19315
zengzhihua24 <765795635@qq.com>
2019-10-18 02:00:22 +0800

    063

---
+++ src/_063_UniquePathsII.java
@@ -0,0 +1,50 @@
+public class _063_UniquePathsII {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else if (i == 0 && j > 0) {
+                    dp[i][j] += dp[i][j - 1];
+                } else if (j == 0 && i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) return 0;
+        int n = obstacleGrid[0].length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int[] row : obstacleGrid) {
+            for (int j = 0; j < n; j++) {
+                if (row[j] == 1) {
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] obstacleGrid = new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        Util.printMatrix(obstacleGrid);
+        int res = new _063_UniquePathsII().uniquePathsWithObstacles2(obstacleGrid);
+        System.out.println(res);
+    }
+}
