c26a03f5bec787824b15c4ea1cabdea9357bcb82
zengzhihua24 <765795635@qq.com>
2019-10-18 21:57:27 +0800

    069

---
+++ src/_069_SqrtX.java
@@ -0,0 +1,21 @@
+public class _069_SqrtX {
+
+    public int mySqrt(int x) {
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid < x / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new _069_SqrtX().mySqrt(8));
+    }
+}
