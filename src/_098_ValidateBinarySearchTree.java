2a9c86626d310fa8915179c202b49be9ddb6c42e
zengzhihua24 <765795635@qq.com>
2019-12-04 01:03:45 +0800

    098

---
+++ src/_098_ValidateBinarySearchTree.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class _098_ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+        System.out.println(new _098_ValidateBinarySearchTree().isValidBST(root));
+    }
+}
