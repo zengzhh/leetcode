4f49fd9ce4ea789ea521e795e1db63922d1c4446
zengzhh <zengzhh@chinatelecom.cn>
2020-10-20 00:23:01 +0800

    105

---
+++ src/_105_ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class _105_ConstructBinaryTreefromPreorderandInorderTraversal {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int preLen = preorder.length;
+        int inLen = inorder.length;
+        Map<Integer, Integer> map = new HashMap<>(preLen);
+        for (int i = 0; i < inLen; i++) {
+            map.put(inorder[i], i);
+        }
+        return buildTree(preorder, 0, preLen - 1, map, 0, inLen - 1);
+    }
+
+    private TreeNode buildTree(int[] preorder, int preLeft, int preRight, Map<Integer, Integer> map, int inLeft, int inRight) {
+        if (preLeft > preRight || inLeft > inRight) {
+            return null;
+        }
+        int rootVal = preorder[preLeft];
+        TreeNode root = new TreeNode(rootVal);
+        int pIndex = map.get(preorder[preLeft]);
+        root.left = buildTree(preorder, preLeft + 1, pIndex - inLeft + preLeft, map, inLeft, pIndex - 1);
+        root.right = buildTree(preorder, pIndex - inLeft + preLeft + 1, preRight, map, pIndex + 1, inRight);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        TreeNode root = new _105_ConstructBinaryTreefromPreorderandInorderTraversal().buildTree(preorder, inorder);
+        Util.printTreeNodePostorder(root);
+    }
+}
