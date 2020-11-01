f12bca4614fb8d3e120fc343dd6ca91e6b31508b
zengzhihua24 <765795635@qq.com>
2020-11-01 11:31:37 +0800

    110_2

--- src/_110_BalancedBinaryTree.java
+++ src/_110_BalancedBinaryTree.java
@@ -1,14 +1,27 @@
 public class _110_BalancedBinaryTree {
 
-    public boolean isBalanced(TreeNode root) {
+/*    public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
-        if (Math.abs(depth(root.left) - depth(root.right)) > 1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        return (Math.abs(depth(root.left) - depth(root.right)) <= 1) && isBalanced(root.left) && isBalanced(root.right);
     }
 
     private int depth(TreeNode root) {
         if (root == null) return 0;
         return Math.max(depth(root.left), depth(root.right)) + 1;
+    }*/
+
+    public boolean isBalanced(TreeNode root) {
+        return depth(root) >= 0;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = depth(root.left);
+        int rightDepth = depth(root.right);
+        if (leftDepth == -1 || rightDepth == -1 || Math.abs(leftDepth - rightDepth) > 1) {
+            return -1;
+        }
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 
     public static void main(String[] args) {
